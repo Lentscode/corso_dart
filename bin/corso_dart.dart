14a46125304ecbbaeaf3a7bef0ebb064cca555ca
Lentscode <antonio.lentini2004@gmail.com>
2024-06-27 12:37:54 +0200

    Aggiornato

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,10 +1,14 @@
 /*
 
-  - stringhe in dettaglio
   - aggiunta caratteri speciali
+  - stringhe in dettaglio
   - recupero carattere in base a posizione
   - metodi stringhe
 
 */
 
-void main(List<String> arguments) {}
+void main(List<String> arguments) {
+  String x = 'Come stai? Io bene, tu?';
+
+  print(x.split(' '));
+}
