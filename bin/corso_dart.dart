aeb1f97deaa62dda1f799c171b54fbb7e292d133
Lentscode <antonio.lentini2004@gmail.com>
2024-07-01 08:30:28 +0200

    Completato

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,52 +1,29 @@
 void main(List<String> arguments) {
-  // Una lista è un'insieme di elementi dello stesso tipo ordinati.
-  // Gli elementi di una lista sono separati da una virgola e racchiusi da parentesi quadre
-  List<String> saluti = ["Ciao", "Hello", "Hola"];
+  // Le [Map] sono collezioni di elementi, detti valori, a cui viene assegnato
+  // un identificativo, detto chiave. Chiave e valore possono essere di qualsiasi
+  // tipo.
+  //* Ovviamente due valori non dovrebbero avere la stessa chiave.
+  Map<String, int> eta = {
+    //. Abbiamo chiavi di tipo [String] e valori [int]
+    "Antonio": 19,
+    "Sara": 25,
+    "Rosario": 67,
+  };
 
-  // Se vogliamo accedere a un elemento della lista possiamo farlo attraverso il
-  // suo indice, ovvero la sua posizione, racchiusa tra parentesi quadre.
-  print(saluti[1]); //? Stampa "Hello", visto che si comincia a contare da 0.
-  print(saluti[3]); //! Da errore, siccome non esiste l'elemento in posizione 3, la lista non è lunga abbastanza
+  // Per recuperare un valore ci basta usare la sua chiave.
+  final etaSara = eta["Sara"]; //! Se non dovesse trovare l'elemento, ci ritornerebbe `null`.
 
-  // Se si vuole modificare un elemento si fa sempre attraverso il suo indice.
-  saluti[1] = "Hi";
+  // Per recuperare chiavi e valori separatamente...
+  final keys = eta.keys;
+  final values = eta.values;
 
-  // Per aggiungere un elemento si usa il metodo add() o addAll() (in caso si
-  // voglia aggiungere una lista di elementi).
-  saluti.add("Hallo");
+  // Per recuperarli insieme...
+  final entries = eta.entries;
+  // che ci da un [Iterable] di tutti gli elementi della mappa.
 
-  //. Tutto ciò funziona anche se la lista è `final`
-  //! tranne riassegnare completamente la lista.
-
-  // -----------------------------------------------------------------------
-
-  // Tra i metodi delle liste più importanti abbiamo map(), che
-  // ritorna un'[Iterable] che contiene tutti gli elementi della lista vecchia
-  // con elementi modificati secondo un pattern.
-  final List<String> salutiMaiuscolo = saluti
-      .map((saluto) => saluto.toUpperCase())
-      .toList(); //? Se vogliamo avere una lista chiamiamo il metodo toList()
-
-  // Un altro metodo è where() che ritorna un'[Iterable] con tutti gli elementi che rispettano una certa condizione
-  final Iterable<String> salutiConH = saluti.where((saluto) => saluto.toLowerCase().contains('h'));
-
-  // -----------------------------------------------------------------------
-
-  // Se iterare tra gli elementi della lista, eseguendo del codice con ognuno di essi,
-  // possiamo usare un for loop con una sintassi speciale
-  for (String saluto in saluti) {
-    print(saluto);
-  }
-
-  // -----------------------------------------------------------------------
-
-  // I [Set] sono simili alle liste, in cui gli elementi sono racchiusi da parentesi graffe,
-  Set<int> numeri = {9, 5, 3, 8};
-
-  // ma sono degli insiemi di elementi
-  // - non ordinati, quindi non si può ottenere un elemento in base al suo indice
-  numeri[1]; //! Da errore
-  // - in cui un elemento non può apparire più di una volta
-  numeri.add(3);
-  print(numeri); //. Il 3 spunterà una sola volta, non avrà un doppione.
+  // Per modificare un valore, si fa sempre tramite la sua chiave.
+  //* Se non è presente un valore con la chiave specificata, viene creato
+  //* un nuovo elemento con questa chiave e con questo valore
+  eta["Rosario"] = 73;
+  eta["Alberto"] = 37; //. Viene aggiunto alla mappa {"Alberto": 37}
 }
