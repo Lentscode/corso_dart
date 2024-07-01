2a94d78337174eaf110d59db2534127a6f2512bc
Lentscode <antonio.lentini2004@gmail.com>
2024-07-01 20:37:11 +0200

    Aggiornato

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,16 +1,8 @@
-import 'package:corso_dart/auto.dart';
+/*
 
-void main(List<String> arguments) {
-  // Per creare un oggetto di tipo [Auto], si usa la seguente sintassi:
-  final fiatPunto =
-      Auto("Fiat", "Punto", 2001); //. Viene eseguito _accensione() visto che si trova nel body del costruttore
+  - getters e setters
+  - proprietà e metodi statici
 
-  // Per accedere a proprietà e metodi si usa l'operatore punto.
-  print(fiatPunto.marca);
-  fiatPunto.accelera();
+*/
 
-  // Qui usiamo un costruttore nominale
-  final audiA1 = Auto.audi(modello: "A1", anno: 2020);
-
-  final porsche911 = Auto.dEpoca(anno: 1976, marca: "Porsche", modello: "911"); //! Avremo un errore perché 1976 non è abbastanza per essere d'epoca
-}
+void main(List<String> arguments) {}
