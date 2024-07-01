093a4ff2f33b7d22f9caa9cd3f65e4b88fbb6900
Lentscode <antonio.lentini2004@gmail.com>
2024-07-01 20:23:24 +0200

    Completato

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,10 +1,16 @@
-/*
+import 'package:corso_dart/auto.dart';
 
-  - cosa sono classi
-  - proprietà e metodi
-  - invocazione
-  - costruttori, lista di inizializzazione, corpo del costruttore
+void main(List<String> arguments) {
+  // Per creare un oggetto di tipo [Auto], si usa la seguente sintassi:
+  final fiatPunto =
+      Auto("Fiat", "Punto", 2001); //. Viene eseguito _accensione() visto che si trova nel body del costruttore
 
-*/
+  // Per accedere a proprietà e metodi si usa l'operatore punto.
+  print(fiatPunto.marca);
+  fiatPunto.accelera();
 
-void main(List<String> arguments) {}
+  // Qui usiamo un costruttore nominale
+  final audiA1 = Auto.audi(modello: "A1", anno: 2020);
+
+  final porsche911 = Auto.dEpoca(anno: 1976, marca: "Porsche", modello: "911"); //! Avremo un errore perché 1976 non è abbastanza per essere d'epoca
+}
