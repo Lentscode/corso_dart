7b93d66768df1111b9cd1663e637b52478a378bd
Lentscode <antonio.lentini2004@gmail.com>
2024-07-03 18:36:21 +0200

    Aggiornato

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,8 +1,17 @@
 /*
 
-  - getters e setters
-  - proprietà e metodi statici
+  - ereditarietà: `extends`
+  - implementazione: `implements`
+  - mixin
 
 */
 
-void main(List<String> arguments) {}
+import 'package:corso_dart/corso_dart.dart';
+
+void main(List<String> arguments) {
+  final fiatPanda = Auto("Fiat", "Panda", 67);
+  final barca1 = Barca("barca a vela", 7, 3.5);
+
+  fiatPanda.suonaClacson();
+  barca1.suonaClacson();
+}
