200fc2fd672888d85982e247a16a66da908dcf63
Lentscode <antonio.lentini2004@gmail.com>
2024-07-04 14:56:14 +0200

    Aggiornato

--- bin/corso_dart.dart
+++ bin/corso_dart.dart
@@ -1,1 +1,25 @@
-void main(List<String> arguments) {}
+import 'package:corso_dart/corso_dart.dart';
+
+void main(List<String> arguments) {
+  //* Usiamo il blocco `try` per "provare" ad eseguire del codice.
+  // In caso di errore, il flusso del programma viene interrotto.
+  try {
+    final result = divide(5, 0);
+    print(result);
+
+    //* Possiamo intercettare un'eccezione specifica usando la keyword `on`.
+    // Se ne abbiamo bisogno, possiamo anche accedere all'errore tramite `catch`.
+  } on DivideByZeroException catch (e) {
+    print("Non puoi dividere un numero per 0 $e");
+
+    //* `catch` intercetta qualsiasi tipo di errore.
+  } catch (e) {
+    print("Errore: $e");
+    rethrow; //? Questa keyword rilancia la stessa eccezione intercettata
+
+    //* Nel blocco `finally`, inseriamo codice che vogliamo che si esegua in caso 
+    //* di errore e non.
+  } finally {
+    print("Se non l'avessi capito, non puoi dividere per 0");
+  }
+}
--- lib/corso_dart.dart
+++ lib/corso_dart.dart
@@ -1,3 +1,11 @@
-int calculate() {
-  return 6 * 7;
+// In questa funzione d'esempio, ritorniamo la divisione con resto tra due interi.
+// Se però il divisore è 0, non possiamo eseguire l'operazione, quindi per prevenirlo
+// lanciamo un'eccezione creata da noi.
+int divide(int a, int b) {
+  if (b == 0) {
+    throw DivideByZeroException(); //. In Dart, può essere lanciato qualsiasi cosa.
+  }
+  return a ~/ b;
 }
+
+class DivideByZeroException {}
