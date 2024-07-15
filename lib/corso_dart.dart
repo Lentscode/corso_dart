028e8defb0609e7df40a13b348ce58e25194a50e
Lentscode <antonio.lentini2004@gmail.com>
2024-07-15 11:53:03 +0200

    Update

--- lib/corso_dart.dart
+++ lib/corso_dart.dart
@@ -1,3 +1,12 @@
+// La keyword `typedef` ci permette di realizzare dei type aliases,
+// ovvero dei tipi da usare al posto di altri tipi.
+typedef Stringa = String;
+// È molto utile in caso abbiamo tipi molto lunghi da inserire come parametri
+// in funzioni o come proprietà in classi.
 typedef Operazione = int Function(int a, int b);
 
-void eseguiOperazione(int a, int b, Operazione operazione) {}
+void eseguiOperazione(int a, int b, Operazione operazione) {
+  final risultato = operazione(a, b);
+
+  print(risultato);
+}
